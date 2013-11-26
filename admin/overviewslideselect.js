536ef796250d76c5bb059640d54222f9ec1b30e8
Maxim <maxim.zavadskiy@metropolia.fi>
2013-11-26 11:27:56 +0200

    cleanup for slideselect

--- admin/overviewslideselect.js
+++ admin/overviewslideselect.js
@@ -1,16 +1,12 @@
 function overviewslideselect(callback){
 	editorwindow.call(this);
 
-	this.setSize(550,600);
+	this.setSize(300,400);
 	this.setPosition('center');
-	this.applyOverlay();
-
 	this.setHeader('overviewslide','select');
 	
 	this._targetinput = new targetDropInput();
 	this.append(this._targetinput.getElement());
-	
-	this._overlay.hide(); //removing overlay effect, so we can drop other pages
 	
 	this.addToolbar();
 	this.addTool('img/check-alt.png','save','save and exit');	
